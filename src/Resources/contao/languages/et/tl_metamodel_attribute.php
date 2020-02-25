06c627427acab27b3c26a87ea84e02087afd046d
Mini Model <minimodel@metamodel.me>
2020-02-25 22:32:47 +0100

    Updated translations from transifex.

--- src/Resources/contao/languages/et/tl_metamodel_attribute.php
+++ src/Resources/contao/languages/et/tl_metamodel_attribute.php
@@ -1,17 +1,28 @@
 <?php
 /**
- * Translations are managed using Transifex. To create a new translation
+ * This file is part of MetaModels/attribute_alias.
+ *
+ * (c) 2012-2020 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * Translations are managed automatically using Transifex. To create a new translation
  * or to help to maintain an existing one, please register at transifex.com.
  *
- * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ * Last-updated: 2020-02-25T22:32:35+01:00
  *
- * @link https://www.transifex.com/signup/
- * @link https://www.transifex.com/projects/p/$$project$$/language/et/
- *
- * last-updated: 2018-02-06T02:11:28+01:00
+ * @copyright 2012-2020 The MetaModels team.
+ * @license   https://github.com/MetaModels/attribute_alias/blob/master/LICENSE LGPL-3.0-or-later
+ * @link      https://www.transifex.com/metamodels/public/
+ * @link      https://www.transifex.com/signup/?join_project=metamodels
  */
 
+
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['alias_fields']['0']           = 'Aliase väljad';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['display_legend']              = 'Vaate seaded';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['field_attribute']             = 'Omadused';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['select_values']['attributes'] = 'Omadused';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['alias']        = 'Alias';
--- src/Resources/contao/languages/fr/tl_metamodel_attribute.php
+++ src/Resources/contao/languages/fr/tl_metamodel_attribute.php
@@ -1,18 +1,29 @@
 <?php
 /**
- * Translations are managed using Transifex. To create a new translation
+ * This file is part of MetaModels/attribute_alias.
+ *
+ * (c) 2012-2020 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * Translations are managed automatically using Transifex. To create a new translation
  * or to help to maintain an existing one, please register at transifex.com.
  *
- * @link http://help.transifex.com/intro/translating.html
- * @link https://www.transifex.com/projects/p/metamodels/language/fr/
+ * Last-updated: 2020-02-25T22:32:35+01:00
  *
- * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
- *
- * last-updated: 2018-02-06T02:11:28+01:00
+ * @copyright 2012-2020 The MetaModels team.
+ * @license   https://github.com/MetaModels/attribute_alias/blob/master/LICENSE LGPL-3.0-or-later
+ * @link      https://www.transifex.com/metamodels/public/
+ * @link      https://www.transifex.com/signup/?join_project=metamodels
  */
+
 
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['alias_fields']['0']           = 'Champs alias';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['alias_fields']['1']           = 'Sélectionner un ou plusieurs attributs pour combiner un alias.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['display_legend']              = 'Paramètres d\'affichage';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['field_attribute']             = 'Attributs';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['force_alias']['0']            = 'Forcer la régénération de l\'alias';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['force_alias']['1']            = 'Cocher ceci, si vous voulez que l\'alias soit régénéré à chaque fois que l\'un des champs dépendant est modifié. Notez que ceci invalidera les anciennes url qui sont basées sur l\'alias';
