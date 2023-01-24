b6c770d249c5259fcb2af092228d3e5e2d5f6e24
Ingolf Steinhardt <info@e-spin.de>
2023-01-24 16:32:45 +0100

    Set unique as default at create

---
+++ src/EventListener/SetUniqueAsDefaultListener.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * This file is part of MetaModels/attribute_alias.
+ *
+ * (c) 2012-2023 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    MetaModels/attribute_alias
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2012-2023 The MetaModels team.
+ * @license    https://github.com/MetaModels/attribute_alias/blob/master/LICENSE LGPL-3.0-or-later
+ * @filesource
+ */
+
+namespace MetaModels\AttributeAliasBundle\EventListener;
+
+use ContaoCommunityAlliance\DcGeneral\Contao\RequestScopeDeterminator;
+use ContaoCommunityAlliance\DcGeneral\Contao\View\Contao2BackendView\Event\BuildWidgetEvent;
+
+/**
+ * This class provides the attribute variant activation.
+ */
+class SetUniqueAsDefaultListener
+{
+    /**
+     * Request scope determinator.
+     *
+     * @var RequestScopeDeterminator
+     */
+    private RequestScopeDeterminator $scopeDeterminator;
+
+    /**
+     * Create a new instance.
+     *
+     * @param RequestScopeDeterminator $scopeDeterminator The scope determinator.
+     */
+    public function __construct(
+        RequestScopeDeterminator $scopeDeterminator
+    ) {
+        $this->scopeDeterminator = $scopeDeterminator;
+    }
+
+    /**
+     * Enable widget isunique at create.
+     *
+     * @param BuildWidgetEvent $event The event.
+     *
+     * @return void
+     */
+    public function buildWidget(BuildWidgetEvent $event): void
+    {
+        $model = $event->getModel();
+
+        if (false === $this->scopeDeterminator->currentScopeIsBackend()
+            && null !== $model->getId()
+            && 'isunique' !== $event->getProperty()->getName()
+        ) {
+            return;
+        }
+
+        $model->setProperty('isunique', true);
+    }
+}
